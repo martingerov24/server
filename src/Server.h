45d17740e1a9ec6558f87cc4e3168b46cff6cc7c
Martin Gerov <mgerov@software-supreme.com>
2022-06-08 15:00:19 +0300

    starting point of the Server part

--- src/Server.h
+++ src/Server.h
@@ -1,18 +1,24 @@
 #pragma once
+#include "dep.h"
 #include "../cppzmq/zmq.hpp"
+
 class Server
 {
-	Server(Server& other) = delete;
-	Server operator=(Server& other) = delete;
+	void catchPostRequest(int size);
 public:
 	Server() = default;
+	Server(const Server& other) = delete;
 	Server(Server&& other) noexcept;
-	void bind(const std::string&& port);
-	Server& operator=(Server&& other) noexcept;
-	void testFuncRecvJob(std::vector<std::string>& jsons);
-	void ConvertStringtoJson(std::vector<std::string>& strings);
-	~Server() = default;
+	Server operator=(const Server& other ) = delete;
+	void bind(
+		const std::string& port
+	);
+	Server& operator=(
+		Server&& other
+	) noexcept;
+	void listen();
+	~Server();
 private:
 	zmq::context_t context{ 1 };
-	zmq::socket_t  socket{ context, zmq::socket_type::rep};
+	zmq::socket_t  socket{ context, zmq::socket_type::rep };
 };
--- src/cppFiles/Server.cpp
+++ src/cppFiles/Server.cpp
@@ -1,13 +1,20 @@
-#include "../Server.h"
-
-
-Server::Server(Server&& other) noexcept
-{
+#include "Server.h"
+#include "json.h"
+Server::Server(
+	Server&& other
+) noexcept {
 	std::swap(*this, other);
 }
 
-Server& Server::operator=(Server&& other) noexcept
+Server::~Server()
 {
+	socket.close();
+	context.close();
+}
+
+Server& Server::operator=(
+	Server&& other
+) noexcept {
 	if (this != &other)
 	{
 		std::swap(*this, other);
@@ -15,37 +22,52 @@ Server& Server::operator=(Server&& other) noexcept
 	return *this;
 }
 
-void Server::bind(const std::string&& port)
-{
+void Server::bind(
+	const std::string& port
+) {
 	socket.bind(port);
 }
 
-void Server::testFuncRecvJob(std::vector<std::string>& jsons)
-{
-	// int count = static_cast<int>(JsonSend::Count);
-	// jsons.resize(static_cast<int>(JsonSend::Count));
-	// for (int i = 0; i < (int)JsonSend::Count; i++)
-	// {
-	// 	zmq::message_t request;
+void Server::catchPostRequest(
+	int size // size in bytes
+) {
+	std::vector<int> tensor(size / 4, 0);
+	//PackageSize = global const variable, defined in dep.h
+	int iterations = (size / sizeof(int32_t) + PackageSize - 1) / PackageSize;
 
-	// 	//  Wait for next request from client
-	// 	socket.recv(&request);
-	// 	std::cout << request << std::endl;
-	// 	jsons[i] = request.to_string();
-	// 	//  Do some 'work'
-	// 	Sleep(1);
+	for (int i = 0; i < iterations; i++){
+		zmq_msg_t buffer;
+		int nbytes = zmq_msg_init(&buffer);
+		nbytes = socket.recv(&buffer, PackageSize);
+		if(nbytes == -1){
+			printf("socket did not receive anything\n");
+			return;
+		}
+		// std::vector<int> msmTemp(zmq_msg_size(&buffer));
+		// std::memcpy(msmTemp.data(), zmq_msg_data(&buffer), zmq_msg_size(&buffer));
+		// tensor.insert(tensor.end(), std::begin(msmTemp), std::end(msmTemp));
 
-	// 	//  Send reply back to client
-	// 	zmq::message_t reply(10);
-	// 	memcpy((void*)reply.data(), "not world", 10);
-	// 	socket.send(reply, zmq::send_flags::none);
-	// }
+		//nightly likely not to work, but if it does, it would make 1 copy instead of
+		// allocation and 2 copies
+		tensor.insert(tensor.end(), zmq_msg_get(&buffer, 0), zmq_msg_size(&buffer));
+	}
 }
+void Server::listen() {
+	zmq::message_t msg;
+	int32_t sizeOfIncommingBuffer;
+	HTTPReq request = HTTPReq::None;
 
-void Server::ConvertStringtoJson(std::vector<std::string>& strings)
-{
-	// for (int i = 0; i < (int)JsonSend::Count; i++)
-	// {
-	// 	json.jsonRecieve(strings[i]);
-	// }
+	//  Wait for next request from client
+	int size = 	socket.recv(&msg);
+	int sizeOfBufferInBytes = jsonSend::getReqAndSize(
+		msg.to_string(),
+	 	request
+	);
+	if (sizeOfBufferInBytes == -1) {
+		//TODO: here you should request
+		//to the client for another message
+		return;
+	}
+	catchPostRequest(sizeOfBufferInBytes);
+	//loop here
 }
